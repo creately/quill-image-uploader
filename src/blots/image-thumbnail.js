11743185b7fe171e21f3bed3f89c162de89ac71c
thisunravisara <thisunravisara@gmail.com>
2021-03-25 11:52:32 +0530

    Added image thumbnail blot

---
+++ src/blots/image-thumbnail.js
@@ -0,0 +1,25 @@
+import Quill from "quill";
+import FileEmbedBlot from "./file-embed.js";
+const Embed = Quill.import("blots/embed");
+
+class ImageThumbnailBlot extends FileEmbedBlot {
+	static createBlot(node, data) {
+        node.setAttribute("data-filedata", JSON.stringify( data ));
+        const container = document.createElement("span");
+        container.className = 'image-thumbnail-blot';
+        container.innerHTML = `
+            <div class="image-thumbnail-div" style="background-image: url(${data.link})"></div>
+            <span class="remove-thumbnail file-embed-option" data-id="remove">x</span>`;
+        node.appendChild(container);
+		return node;
+	}
+}
+
+ImageThumbnailBlot.blotName = "imageThumbnail";
+ImageThumbnailBlot.tagName = "span";
+ImageThumbnailBlot.className = "image-thumbnail";
+
+
+Quill.register({ "formats/imageThumbnail": ImageThumbnailBlot });
+
+export default ImageThumbnailBlot;
